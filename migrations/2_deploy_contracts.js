82f880ef21ed4c2cb02868e79fedbf877ec26805
asfwan <asfwan@gmail.com>
2021-09-15 22:19:02 +0800

    adoption tutorial completed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var Monster = artifacts.require("./MonsterContract.sol");
+var Monster = artifacts.require("MonsterContract");
+var Adoption = artifacts.require("Adoption");
 
 module.exports = function(deployer) {
   deployer.deploy(Monster);
+  deployer.deploy(Adoption);
 };
