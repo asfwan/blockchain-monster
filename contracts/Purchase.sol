c23602461633bea5f3ea6841625f818448df6830
asfwan <asfwan@gmail.com>
2021-09-16 12:30:54 +0800

    renamed contract

---
+++ contracts/Purchase.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Purchase {
+    address[16] public purchasers;
+
+    // Adopting a pet
+    function purchase(uint konflixId) public returns (uint) {
+        require(konflixId >= 0 && konflixId <= 15);
+
+        purchasers[konflixId] = msg.sender;
+
+        return konflixId;
+    }
+
+    // Retrieving the purchasers
+    function getPurchasers() public view returns (address[16] memory) {
+        return purchasers;
+    }
+}
