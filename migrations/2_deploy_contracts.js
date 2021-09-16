a3623c83c5d5ad6d9b406031f77a7725417ff609
asfwan <asfwan@gmail.com>
2021-09-16 13:03:35 +0800

    not deploying monster contract for ropsten

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
-var Monster = artifacts.require("MonsterContract");
+// var Monster = artifacts.require("MonsterContract");
 var Purchase = artifacts.require("Purchase");
 
 module.exports = function(deployer) {
-  deployer.deploy(Monster);
+  // deployer.deploy(Monster);
   deployer.deploy(Purchase);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,12 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    ropsten:  {
+      network_id: 3,
+      host: "localhost",
+      port:  8545,
+      gas:   2900000
+    },
     develop: {
       port: 8545
     }
